a7312d91aac83456c7e19fce262552351b6df4bf
Arthur Bogiano <35048860+ArthurBogiano@users.noreply.github.com>
2021-01-19 11:16:29 -0300

    Alias para diminuir alguns comandos frequentes

---
+++ alias.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+alias gcommum='gobuster dir -elx php,txt,sql,db,asp,swp,bkp,old -a "Firefox/8.5.4" -w /usr/share/seclists/Discovery/Web-Content/common.txt '
+alias gmedium='gobuster dir -elx php,txt,sql,db,asp,swp,bkp,old -a "Firefox/8.5.4" -w /usr/share/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt '
