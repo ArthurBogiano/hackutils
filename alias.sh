88dc064070278ad71526d114e720c849f82bf17e
Arthur Bogiano <35048860+ArthurBogiano@users.noreply.github.com>
2021-02-05 18:41:01 -0300

    Next tor

--- alias.sh
+++ alias.sh
@@ -2,3 +2,4 @@
 
 alias gcommum='gobuster dir -elx php,txt,sql,db,asp,swp,bkp,old -a "Firefox/8.5.4" -w /usr/share/seclists/Discovery/Web-Content/common.txt '
 alias gmedium='gobuster dir -elx php,txt,sql,db,asp,swp,bkp,old -a "Firefox/8.5.4" -w /usr/share/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt '
+alias tornext='killall -HUB tor '
