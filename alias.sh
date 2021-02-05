d1a896e6f699201b321ad15d2cfdad2e95edafb9
Arthur Bogiano <35048860+ArthurBogiano@users.noreply.github.com>
2021-02-05 19:10:09 -0300

    tor bug

--- alias.sh
+++ alias.sh
@@ -2,6 +2,6 @@
 
 alias gcommum='gobuster dir -elx php,txt,sql,db,asp,swp,bkp,old -a "Firefox/8.5.4" -w /usr/share/seclists/Discovery/Web-Content/common.txt '
 alias gmedium='gobuster dir -elx php,txt,sql,db,asp,swp,bkp,old -a "Firefox/8.5.4" -w /usr/share/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt '
-alias tornext='killall -HUB tor '
+alias tornext='killall -HUP tor '
 alias feroxc='feroxbuster -x php,txt,sql,db,asp,swp,bkp,old -a "Firefox/8.5.4" -w /usr/share/seclists/Discovery/Web-Content/common.txt '
 alias feroxm='feroxbuster -x php,txt,sql,db,asp,swp,bkp,old -a "Firefox/8.5.4" -w /usr/share/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt '
