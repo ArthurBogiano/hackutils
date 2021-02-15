e949a8720c402df4a92d18f4891f2c3189e6b94a
Arthur Bogiano <35048860+ArthurBogiano@users.noreply.github.com>
2021-02-15 13:07:18 -0300

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -7,4 +7,4 @@ alias feroxc='feroxbuster -x php,txt,sql,db,asp,swp,bkp,old -a "Firefox/8.5.4" -
 alias feroxm='feroxbuster -x php,txt,sql,db,asp,swp,bkp,old -a "Firefox/8.5.4" -w /usr/share/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt '
 
 alias fcommum='ffuf -w /usr/share/seclists/Discovery/Web-Content/common.txt '
-alias fmedium='ffuf -w /usr/share/seclists/Discovery/Web-content/directory-list-2.3-medium.txt'
+alias fmedium='ffuf -w /usr/share/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt'
