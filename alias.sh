6a696af78e70a207f5544551e8bed4ee7056e958
Arthur Bogiano <35048860+ArthurBogiano@users.noreply.github.com>
2023-06-14 14:39:05 -0300

    colors

--- alias.sh
+++ alias.sh
@@ -6,8 +6,8 @@ alias tornext='killall -HUP tor '
 alias feroxc='feroxbuster -x php,txt,sql,db,asp,swp,bkp,old -a "Firefox/8.5.4" -w /usr/share/seclists/Discovery/Web-Content/common.txt '
 alias feroxm='feroxbuster -x php,txt,sql,db,asp,swp,bkp,old -a "Firefox/8.5.4" -w /usr/share/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt '
 
-alias fcommum='ffuf -w /usr/share/seclists/Discovery/Web-Content/common.txt '
-alias fmedium='ffuf -w /usr/share/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt'
-alias fraftd='ffuf -w /usr/share/seclists/Discovery/Web-Content/raft-large-directories.txt'
-alias fraftf='ffuf -w /usr/share/seclists/Discovery/Web-Content/raft-large-files.txt'
+alias fcommum='ffuf -w /usr/share/seclists/Discovery/Web-Content/common.txt' -c
+alias fmedium='ffuf -w /usr/share/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt' -c
+alias fraftd='ffuf -w /usr/share/seclists/Discovery/Web-Content/raft-large-directories.txt' -c
+alias fraftf='ffuf -w /usr/share/seclists/Discovery/Web-Content/raft-large-files.txt' -c
 
