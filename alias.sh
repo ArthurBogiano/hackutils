e2c19261e212ac37cca91b5cca0c3849b065256c
Arthur Bogiano <35048860+ArthurBogiano@users.noreply.github.com>
2023-08-09 18:38:26 -0300

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -16,3 +16,10 @@ alias fptbr='ffuf -w /usr/share/seclists/Discovery/Web-Content/common-and-portug
 alias fhell0='ffuf -w /usr/share/seclists/Discovery/Web-Content/hell0.txt -c '
 
 alias hell0mount='touch /tmp/hell0; cat /usr/share/seclists/Discovery/Web-Content/common.txt >> /tmp/hell0; cat /usr/share/seclists/Discovery/Web-Content/raft-large-directories.txt >> /tmp/hell0; cat /usr/share/seclists/Discovery/Web-Content/common-and-portuguese.txt >> /tmp/hell0; cat /usr/share/seclists/Discovery/Web-Content/quickhits.txt >> /tmp/hell0; cat /usr/share/seclists/Fuzzing/fuzz-Bo0oM.txt >> /tmp/hell0; cat /usr/share/seclists/Discovery/Web-Content/raft-large-files.txt >> /tmp/hell0; sort -u /tmp/hell0 > /usr/share/seclists/Discovery/Web-Content/hell0.txt; rm /tmp/hell0'
+
+alias nhell0='nmap -sV -v '
+alias nhell1='nmap -sSV -v '
+alias nhell2='nmap -sSV -v --scan-delay 1000ms '
+alias nhell3='nmap -sSV -v --scan-delay 1000ms -g 80 '
+alias nhell4='nmap -sSV -v --scan-delay 1000ms -D RND:10 -g 80 '
+alias nhell4='nmap -sSV -v --scan-delay 1000ms -D RND:20 -g 80 -T4 '
