32e92fdfe08b3a721c5e29c9f52a74e35f49199e
Arthur Bogiano <35048860+ArthurBogiano@users.noreply.github.com>
2023-08-18 13:33:38 -0300

    Create mywordlist.sh

---
+++ mywordlist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+touch /tmp/hell0;
+cat /usr/share/seclists/Discovery/Web-Content/common.txt >> /tmp/hell0;
+cat /usr/share/seclists/Discovery/Web-Content/raft-large-directories.txt >> /tmp/hell0;
+cat /usr/share/seclists/Discovery/Web-Content/common-and-portuguese.txt >> /tmp/hell0;
+cat /usr/share/seclists/Discovery/Web-Content/quickhits.txt >> /tmp/hell0;
+cat /usr/share/seclists/Fuzzing/fuzz-Bo0oM.txt >> /tmp/hell0;
+cat /usr/share/seclists/Discovery/Web-Content/raft-large-files.txt >> /tmp/hell0;
+cat /usr/share/seclists/Discovery/Web-Content/IIS.fuzz.txt >> /tmp/hell0;
+cat /usr/share/seclists/Discovery/Web-Content/swagger.txt >> /tmp/hell0;
+sort -u /tmp/hell0 > /usr/share/seclists/Discovery/Web-Content/hell0.txt;
+rm /tmp/hell0
