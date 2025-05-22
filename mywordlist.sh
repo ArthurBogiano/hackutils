e6f1b868fbc2513622337e2ba2dbf5b326cc64e2
Arthur Bogiano <35048860+ArthurBogiano@users.noreply.github.com>
2025-05-22 16:03:25 -0300

    Update mywordlist.sh

--- mywordlist.sh
+++ mywordlist.sh
@@ -7,7 +7,10 @@ cat /usr/share/seclists/Discovery/Web-Content/common-and-portuguese.txt >> /tmp/
 cat /usr/share/seclists/Discovery/Web-Content/quickhits.txt >> /tmp/hell0;
 cat /usr/share/seclists/Fuzzing/fuzz-Bo0oM.txt >> /tmp/hell0;
 cat /usr/share/seclists/Discovery/Web-Content/raft-large-files.txt >> /tmp/hell0;
-cat /usr/share/seclists/Discovery/Web-Content/IIS.fuzz.txt >> /tmp/hell0;
-cat /usr/share/seclists/Discovery/Web-Content/swagger.txt >> /tmp/hell0;
+cat /usr/share/seclists/Discovery/Web-Content/Web-Servers/IIS.txt >> /tmp/hell0;
+cat /usr/share/seclists/Discovery/Web-Content/Web-Servers/Apache.txt >> /tmp/hell0;
+cat /usr/share/seclists/Discovery/Web-Content/Web-Servers/nginx.txt >> /tmp/hell0;
+cat /usr/share/seclists/Discovery/Web-Content/Web-Servers/JBoss.txt >> /tmp/hell0;
+cat /usr/share/seclists/Discovery/Web-Content/Service-Specific/Swagger.txt >> /tmp/hell0;
 sort -u /tmp/hell0 > /usr/share/seclists/Discovery/Web-Content/hell0.txt;
 rm /tmp/hell0
